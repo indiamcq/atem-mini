50b1100c70969d12e381c952da62a5d15ab25adf
indiamcq <india.mcq@gmail.com>
2022-10-13 23:01:16 +1000

    Added comment documentation

--- Companion/js/server2.js
+++ Companion/js/server2.js
@@ -1,3 +1,4 @@
+// Setup the osc-receiver server
 var OscReceiver = require('osc-receiver')
   , receiver = new OscReceiver();
 const { exec } = require("child_process");
@@ -6,10 +7,15 @@ receiver.bind(9337);
 
 receiver.on('message', function() {
   // handle all messages
+  // get the address
 var address = arguments[0];
+  // assign the OSC variables to an array
 var args = Array.prototype.slice.call(arguments, 1);
+  // From the array create space separated command line arguments, acceptable to all Windows programs.
 var part = replaceAll(args.toString(),","," ")
+  // Write all the arguments out for reference. Could be commented out if all is working well.
 console.log(arguments);
+  // Handle address variations or output Unknown
 switch (address) {
 	case '/script':
 		var a = "C:/Users/Public/Companion/runscript.cmd " + part;
@@ -36,7 +42,8 @@ switch (address) {
 });
 
 function myCmd(a) {
-	console.log("Command: " + a  + '\n')
+	// Run the command line for each address type
+	console.log("Command: " + a )
 	exec( a , (error, stdout, stderr) => {
 		if (error) {
 		console.log(`error: ${error.message}`);
@@ -51,5 +58,6 @@ function myCmd(a) {
 	return;
 }
 function replaceAll(str, find, replace) {
+	// do a replaceall instead of only replace the first occurance
   return str.replace(new RegExp(find, 'g'), replace);
 }
